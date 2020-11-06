640572e3cb1ac5d5a62bffd8dfa841c0468df139
paulstapor <paul.stapor@helmholtz-muenchen.de>
2020-11-06 16:19:11 +0100

    update test

--- Test/run_tests.sh
+++ Test/run_tests.sh
@@ -30,21 +30,13 @@ time python sbml2amici.py
 # time python sbml2copasi.py # TBD: automated Copasi import
 
 # Run JWS trajectory comparison
-time python compareStateTrajectories_JWS_1.py
-time python compareStateTrajectories_JWS_2.py
-
-# We need to manually copy the trajectory references
-cp -r ../Cache/trajectories_reference_biomodels ../SolverStudyTest/Models
-
-# Run Biomodels trajectory comparison
-time python compareStateTrajectories_BioModels_1.py
-time python compareStateTrajectories_BioModels_2.py
+time python compare_state_trajectories
 
 # Select models to include in main study
-time python selectModelsForMain.py
+time python filter_models_by_error.py
 
 # Run main study
-time python execute_WholeStudy.py
-time python execute_Tolerances.py
+# time python execute_WholeStudy.py
+# time python execute_Tolerances.py
 
 # TODO Run Plots
