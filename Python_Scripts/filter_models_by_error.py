71b63eacae9ad19fb1c15e957daa30d3963546ff
paulstapor <paul.stapor@helmholtz-muenchen.de>
2020-11-06 16:17:25 +0100

    model fitering added

--- Python_Scripts/filter_models_by_error.py
+++ Python_Scripts/filter_models_by_error.py
@@ -12,20 +12,28 @@ model_info = pd.read_csv(os.path.join(DIR_MODELS, 'model_summary.tsv'),
 max_num_errors = pd.read_csv(os.path.join(DIR_MODELS, 'amici_results.tsv'),
                              sep='\t')
 
-def get_matching_models(max_num_errors):
-    accepted = []
-    min_error = []
-    for i_model in max_num_errors.index:
-        tmp = max_num_errors.loc[i_model,:].values
-        tmpmin = np.min(tmp[2:])
-        min_error.append(tmpmin)
-        accepted.append(tmpmin < 1e-4)
+n_submodels = model_info.shape[0]
+model_info = model_info.join(pd.Series(data=['False'] * n_submodels,
+                                       name='accepted'))
+model_info = model_info.join(pd.Series(data=['inf'] * n_submodels,
+                                       name='amici_min_error'))
 
-    max_num_errors = max_num_errors.join(pd.Series(data=accepted, name='accepted'))
-    max_num_errors = max_num_errors.join(pd.Series(data=min_error, name='amici_min_error'))
+accepted = []
+min_error = []
+for i_model in max_num_errors.index:
+    tmp = max_num_errors.loc[i_model,:].values
 
-    return max_num_errors
+    tmpmin = np.min(tmp[2:])
+    acc = tmpmin < 1e-4
+    min_error.append(tmpmin)
+    accepted.append(acc)
 
-max_num_errors = get_matching_models(max_num_errors)
-max_num_errors.to_csv(os.path.join(DIR_MODELS, 'amici_best_sim.tsv'),
-                      sep='\t', index=False)
+    amici_path = max_num_errors.loc[i_model, 'amici_path']
+    model_info.loc[
+        model_info['amici_path'] == amici_path, 'amici_min_error'] = tmpmin
+    model_info.loc[model_info['amici_path'] == amici_path, 'accepted'] = acc
+
+max_num_errors = max_num_errors.join(pd.Series(data=accepted, name='accepted'))
+max_num_errors = max_num_errors.join(pd.Series(data=min_error, name='amici_min_error'))
+
+model_info.to_csv(os.path.join(DIR_MODELS, 'model_summary.tsv'), sep='\t', index=False)
